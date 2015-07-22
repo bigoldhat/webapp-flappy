6dedbff57ce169219ed520d1f7a24aa004de9d15
bigoldhat <bigoldhat@gmail.com>
2015-07-22 10:11:01 +0100

    A new and working version with the large majority of the Day 3 implementations (bonuses etc.). The next release may feature additional bonuses.

--- js/flappy.js
+++ js/flappy.js
@@ -3,6 +3,10 @@
 /* I define a set of global variables before the game is created. */
 /* The first of these dictates the order by which the game runs (I think). */
 var actions = { preload: preload, create: create, update: update };
+/* The game's height and width were once just used to define the size of the game window.
+ * By using variables instead, these values can be referred to later on easily. */
+var width = 700;
+var height = 400;
 /* Following this, we create a new game object in Phaser, which we can begin to interact with. */
 // Phaser parameters:
 // - game width
@@ -10,7 +14,7 @@ var actions = { preload: preload, create: create, update: update };
 // - renderer (go for Phaser.AUTO)
 // - element where the game will be drawn ('game')
 // - actions on the game state (or null for nothing)
-var game = new Phaser.Game(700, 400, Phaser.AUTO, "game", actions);
+var game = new Phaser.Game(width, height, Phaser.AUTO, "game", actions);
 /* The score at the start of the game begins at 0. */
 var score = -3;
 /* A new variable is created which will become an object to contain the score of the current game. */
@@ -27,7 +31,7 @@ var pipeInterval = 1.75;
 /* These variables have been drawn out from existing values in former implementations.*/
 var gameGravity = 200;
 var gameVelocity = -200;
-var jumpPower = 200;
+var jumpPower = -125;
 var pipeGap = 100;
 
 /* Bonuses are a new feature - these arrays store the sprites.*/
@@ -63,12 +67,12 @@ function create() {
     game.stage.setBackgroundColor("#F3D3A3");
     /* At the top and bottom of the screen, blocks are placed.
      * This prevents the user from scoring points by simply dropping under all of the columns of blocks.*/
-    /* A pipe is generated from the same place with a psuedo-random arrangement every time the defined amount of time (pipeInterval) has elapsed. */
-    game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generatePipe);
     for (var count=0; count<16; count++) {
         addPipeBlock(50 * count, 0, false);
         addPipeBlock(50 * count, 395, false);
     }
+    /* A pipe is generated from the same place with a psuedo-random arrangement every time the defined amount of time (pipeInterval) has elapsed. */
+    game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generate);
     /* Two labels are created. The first is rather optional and simply shows a welcome message.
      * The other is the aforementioned label showing the score. */
     labelText = game.add.text(20, 20, "Welcome.",
@@ -94,7 +98,7 @@ function create() {
 function update() {
     /* This line checks if at any point during gameplay there is an overlap between any pipe and player sprites.
      * If this is the case, the game will end. */
-    game.physics.arcade .overlap(player, pipes, gameOver);
+    game.physics.arcade.overlap(player, pipes, gameOver);
     /* The people at CCA used the game's window's height and width here -
      * to determine if the player had passed the bounds of the window.
      * This was as follows:
@@ -103,12 +107,17 @@ function update() {
      * }*/
 
     /* Following this, rotation on the player sprite results by using the trigonometric functions in JS.*/
-    player.rotation = Math.atan(player.body.velocity.y / gameVelocity);
+    /* I very subtly changed the parameter of the atan function to a negative, meaning that the bird will drop with a forwards, not backwards lean if it descends. */
+    player.rotation = Math.atan(-player.body.velocity.y / gameVelocity);
 
     /* After every frame, if there is an intersection between a balloon or a weight, and the player sprite,
     * the gravity should change to replicate a net vertical force change. */
     checkBonus(balloons, -50);
     checkBonus(weights, 50);
+
+    /* I have added this section, meaning that the label chases the player across the map. */
+    labelScore.x = player.x + 50;
+    labelScore.y = player.y;
 }
 
 function checkBonus(bonusArray, bonusEffect) {
@@ -149,60 +158,98 @@ function addPipeEnd(x, y) {
     var pipe = game.add.sprite(x, y, "pipe-end");
     pipes.push(pipe);
     game.physics.arcade.enable(pipe);
-    block.body.velocity.x = -gameSpeed;
+    pipe.body.velocity.x = gameVelocity;
 }
 
 function generate(){
     var diceRoll = game.rnd.integerInRange(1, 10);
     if(diceRoll==1){
-        generateBalloons();
+        generateBonus("balloons");
     } else if(diceRoll==2){
-        generateWeight();
+        generateBonus("weight");
     } else {
         generatePipe();
     }
 }
 
-
 function generatePipe() {
     /* A gap is needed in each column of pipes.
      * The place at which this starts is determined by a random number, and in CCA's example, this gap will be two blocks tall. */
-    /* The definition of the start of the gap has now changed.*/
+    /* The definition of the start of the gap has now changed.
+     * This means that all workings are now outlined in game coordinates from the ground up to streamline it.
+     * pipeGap is integer that was set upon initialisation for the height of the gap between pipes. */
     var gapStart = game.rnd.integerInRange(50, height - 50 - pipeGap);
     /* For each of the eight places in a column available to put a block,
      * a block is placed at certain coordinates along the column if it is not earmarked for the gap. */
-    for (var count = 0; count < 8; count++) {
-        if(count != gapStart && count != gapStart+1){
-            addPipeBlock(750, count * 50, true);
-        }
+    /* This main section of code in this function has also now changed. */
+    /* In this section, an end to the pipe is added where the actual beginning of the gap would have started.
+     * This is replaced with a much stubbier starting pipe.
+     * This means that it assumes the place where the old block would have been with adjustments for its different size.*/
+    addPipeEnd(width-5,gapStart - 25);
+    /* The main blocks are then added above this ending block in a for loop from the end block to the top of the screen.*/
+    for(var y=gapStart - 75; y>-50; y -= 50){
+        addPipeBlock(width, y, true);
+    }
+    /* Likewise, a similar story emerges for the pipe that extends from the bottom of the screen. */
+    /* A cap is added, and then the pipe blocks are added to the bottom of the screen. */
+    addPipeEnd(width-5,gapStart+pipeGap);
+    for(var y=gapStart + pipeGap + 25; y<height; y += 50){
+        addPipeBlock(width, y, true);
     }
     /* A column is created periodically, and likewise the score should be changed periodically.
      * As far as I know, this score change also just happens to occur as the player sprite goes through a gap - a new column must be being created at the same time. */
     changeScore();
 }
 
+/* This entire function is a new addition - adding the bonus sprites for the player. */
+function generateBonus(bonusType){
+    /* A bonus is added at the coordinates width and height.
+     * At first, it seems unusual to deploy a sprite at the very top or bottom of the screen, but these sprites will float upwards or downwards from this position. */
+    if (bonusType == "balloons") {
+        var bonus = game.add.sprite(width, height, bonusType);
+        balloons.push(bonus);
+    } else if (bonusType == "weight") {
+        var bonus = game.add.sprite(width, 0, bonusType);
+        weights.push(bonus);
+    }
+    /* The sprite is added to the list of balloons; has physics enabled; is assigned the same velocity and gravity as the moving pipes...
+    and is assigned to move upwards or downwards of the screen at a psuedorandom velocity. */
+    game.physics.arcade.enable(bonus);
+    bonus.body.velocity.x = gameVelocity;
+    if (bonusType == "balloons") {
+        bonus.body.velocity.y = -game.rnd.integerInRange(60,100);
+    } else if (bonusType == "weight") {
+        bonus.body.velocity.y = game.rnd.integerInRange(60,100);
+    }
+}
+
+/* This function seems pretty self-explanatory. It is launched upon a collision with a bonus sprite. */
+function changeGravity(g){
+    gameGravity += g;
+    player.body.gravity.y = gameGravity;
+}
+
 function playerJump() {
-    /* The player sprite is given a velocity of -200, and it decelerates upwards under gravity in an arc. */
-    player.body.velocity.y = -100;
+    /* The player sprite is given a negative velocity, meaning that it decelerates upwards under the influence of gravity in an arc. */
+    player.body.velocity.y = jumpPower;
+    /* A sound is played. */
     game.sound.play("score");
-    labelScore.x = player.x + 50;
-    labelScore.y = player.y;
 }
 
 function changeScore() {
     /* The score is increased by one and set as the contents of the label's text property. */
     score++;
-    if (score > 0) {
-        labelScore.setText(score.toString());
-    }
+    /* For some reason, the stacks of pipes did not spawn early enough, meaning that the user had a score of about 2-4 before the game even started.
+     * I have fixed this by setting the score in code to -3 (making it act more like a countdown), however this may need to be fixed in future.*/
+    labelScore.setText(score.toString());
 }
 
 function gameOver() {
     /* The game ends upon a collision between pipe and player sprites.
      * This causes the game to stop functioning at in Phaser. */
-    labelText.setText("You lose.");
-    game.destroy();
-    /* JQuery then begins functionality which causes the form to become visible and interactive in HTML. */
+    labelText.setText("Try again.");
+    /* Functionality then begins involving JQuery, causing the form to become visible and interactive in HTML. */
     $("#score").val(score);
     $("#greeting").show();
+    game.destroy();
 }
