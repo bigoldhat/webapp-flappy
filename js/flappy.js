3e5bb5780cb28dbe0c872766dd86d7b729010e8c
bigoldhat <bigoldhat@gmail.com>
2015-07-22 13:00:50 +0100

    Further Day 3 modifications ave been made. A third bonus switches gravity, the background has been changed and editing has been done to the labels.

--- js/flappy.js
+++ js/flappy.js
@@ -1,7 +1,7 @@
 /* This is the game as (more-or-less) obtained from cambridgecoding (with annotations).
 
 /* I define a set of global variables before the game is created. */
-/* The first of these dictates the order by which the game runs (I think). */
+/* The first of these dictates the order by which the game Flys (I think). */
 var actions = { preload: preload, create: create, update: update };
 /* The game's height and width were once just used to define the size of the game window.
  * By using variables instead, these values can be referred to later on easily. */
@@ -19,8 +19,10 @@ var game = new Phaser.Game(width, height, Phaser.AUTO, "game", actions);
 var score = -3;
 /* A new variable is created which will become an object to contain the score of the current game. */
 var labelScore;
-/* A new variable is created which will become an object to contain the score of the current game. */
+/* A new variable is created which will become a label to display user-friendly information. */
 var labelText;
+/* A new variable is created to store a label to display the bonus gravity affecting the player. */
+var labelBonus;
 /* A new variable is created which will become an object that can be used to create player sprites and then change their properties. */
 var player;
 /* A new variable is created which will become an object that can be used to change the properties of any child sprites of the player. */
@@ -29,7 +31,8 @@ var pipes = [];
 var pipeInterval = 1.75;
 
 /* These variables have been drawn out from existing values in former implementations.*/
-var gameGravity = 200;
+var INITIAL_gameGravity = 200;
+var gameGravity = INITIAL_gameGravity;
 var gameVelocity = -200;
 var jumpPower = -125;
 var pipeGap = 100;
@@ -37,6 +40,7 @@ var pipeGap = 100;
 /* Bonuses are a new feature - these arrays store the sprites.*/
 var balloons = [];
 var weights =[];
+var gravitySwitches = [];
 
 /* JQuery obtains the locally stored scores and prints them to the empty unordered list on the HTML page. */
 $.get("/score", function(data){
@@ -51,12 +55,13 @@ $.get("/score", function(data){
 /* The images and audio are loaded to Phaser in this function. */
 function preload() {
     /* Images */
-    game.load.image("playerImg","../assets/flappy.png");
+    game.load.image("playerImg","../assets/flappy-new.png");
     game.load.image("pipe","../assets/pipe2-body.png");
     game.load.image("pipe-border","../assets/pipe2-body2.png");
     game.load.image("pipe-end","../assets/pipe-end.png");
     game.load.image("balloons","../assets/balloons.png");
     game.load.image("weight","../assets/weight.png");
+    game.load.image("gravitySwitch", "../assets/gravitySwitch.png");
     /* Audio */
     game.load.audio("score", "../assets/point.ogg");
 }
@@ -64,7 +69,7 @@ function preload() {
 function create() {
     /* The background colour for the game window is set.
      * This could also be set to an image. */
-    game.stage.setBackgroundColor("#F3D3A3");
+    game.stage.setBackgroundColor("#0B1B1C");
     /* At the top and bottom of the screen, blocks are placed.
      * This prevents the user from scoring points by simply dropping under all of the columns of blocks.*/
     for (var count=0; count<16; count++) {
@@ -75,10 +80,15 @@ function create() {
     game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generate);
     /* Two labels are created. The first is rather optional and simply shows a welcome message.
      * The other is the aforementioned label showing the score. */
-    labelText = game.add.text(20, 20, "Welcome.",
+    /* The welcome message is now one of multiple messages chosen psuedorandomly. */
+    var messages = ["Fly... as far as you can!", "Aren't you supposed to be working?", "Top tip: Don't lose.", "You are reading this.", "This message is invisible when you blink. Seriously."]
+    var message = game.rnd.integerInRange(0, messages.length -1);
+    labelText = game.add.text(100, height-50, messages[message],
         {font: "30px Gloucester MT Extra Condensed", fill: "#777777"});
     labelScore = game.add.text(20, 60, "0",
         {font: "30px Gloucester MT Extra Condensed", fill: "#777777"});
+    labelBonus = game.add.text(20, 60, gameGravity/200 + "g",
+        {font: "18px Gloucester MT Extra Condensed", fill: "#000000"});
     /* A sprite is added and assigned to the player as an object. */
     player = game.add.sprite(80, 200, "playerImg");
     /* The player's sprite is assigned an anchor, from which it can appear to rotate according to its trajectory. */
@@ -91,7 +101,7 @@ function create() {
     game.physics.arcade.enable(player);
     /* A given player sprite at a standstill is accelerated by a simulated gravitational force. */
     player.body.gravity.y = gameGravity;
-    /* Pressing the spacebar will now cause the playerJump() function to run (causing the player to jump). */
+    /* Pressing the spacebar will now cause the playerJump() function to Fly (causing the player to jump). */
     game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
 }
 
@@ -107,17 +117,31 @@ function update() {
      * }*/
 
     /* Following this, rotation on the player sprite results by using the trigonometric functions in JS.*/
-    /* I very subtly changed the parameter of the atan function to a negative, meaning that the bird will drop with a forwards, not backwards lean if it descends. */
     player.rotation = Math.atan(-player.body.velocity.y / gameVelocity);
 
     /* After every frame, if there is an intersection between a balloon or a weight, and the player sprite,
     * the gravity should change to replicate a net vertical force change. */
     checkBonus(balloons, -50);
     checkBonus(weights, 50);
+    checkBonus(gravitySwitches, 0);
 
-    /* I have added this section, meaning that the label chases the player across the map. */
+    /* I have added this section, meaning that the labels chase the player across the map. */
     labelScore.x = player.x + 50;
     labelScore.y = player.y;
+    labelBonus.x = player.x;
+    labelBonus.y = player.y - 50;
+
+    /* I have also added this section.
+     * If the gravity is less than it was initially, the text will be green.
+     * If the gravity is more than it was initially, the text will red.
+     * If the gravity is the same as it was initially, the text will be black. */
+    if (gameGravity < INITIAL_gameGravity) {
+        labelBonus.text.fontcolor = "#009900";
+    } else if (gameGravity > INITIAL_gameGravity) {
+        labelBonus.text.fontcolor = "#009900";
+    } else {
+        labelBonus.text.fontcolor = "#000000";
+    }
 }
 
 function checkBonus(bonusArray, bonusEffect) {
@@ -167,6 +191,8 @@ function generate(){
         generateBonus("balloons");
     } else if(diceRoll==2){
         generateBonus("weight");
+    } else if(diceRoll==3) {
+        generateBonus("gravitySwitch");
     } else {
         generatePipe();
     }
@@ -211,6 +237,9 @@ function generateBonus(bonusType){
     } else if (bonusType == "weight") {
         var bonus = game.add.sprite(width, 0, bonusType);
         weights.push(bonus);
+    } else if (bonusType == "gravitySwitch") {
+        var bonus = game.add.sprite(width, 0, bonusType);
+        gravitySwitches.push(bonus);
     }
     /* The sprite is added to the list of balloons; has physics enabled; is assigned the same velocity and gravity as the moving pipes...
     and is assigned to move upwards or downwards of the screen at a psuedorandom velocity. */
@@ -218,15 +247,27 @@ function generateBonus(bonusType){
     bonus.body.velocity.x = gameVelocity;
     if (bonusType == "balloons") {
         bonus.body.velocity.y = -game.rnd.integerInRange(60,100);
-    } else if (bonusType == "weight") {
+    } else if (bonusType == "weight" || bonusType == "gravitySwitch") {
         bonus.body.velocity.y = game.rnd.integerInRange(60,100);
     }
 }
 
 /* This function seems pretty self-explanatory. It is launched upon a collision with a bonus sprite. */
+/* After expanding this section to account for the sprites which switch gravity, the first section (if...) is related to balloons and weights,
+ * whilst the second is related to gravity switches.*/
 function changeGravity(g){
-    gameGravity += g;
-    player.body.gravity.y = gameGravity;
+    if (g != 0) {
+        gameGravity += g;
+        player.body.gravity.y = gameGravity;
+    } else {
+        gameGravity = -gameGravity;
+        player.body.gravity.y = gameGravity;
+        jumpPower = -jumpPower;
+        /* These modifications are aesthetic, but help the player understand the new gravitational force.*/
+        player.scale.y = -player.scale.y;
+    }
+    /* Irrespective of the resultant gravitational effect, the new quantity is reassigned to the label. */
+    labelBonus.setText(gameGravity/200 + "g");
 }
 
 function playerJump() {
